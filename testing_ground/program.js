524beabc3b1d7db55758fde50ba4278e6fc00971
Aendros <borxinhaa@gmail.com>
2014-09-20 01:12:06 +0200

    Added first example: Hello world!

---
+++ testing_ground/program.js
@@ -0,0 +1,9 @@
+document.writeln('Hello, world!');
+// This is line-ending comments
+
+/*
+And this are multiline
+coments. This comements are not safe, so it's better to
+use the double slash. Just use it on text comments, NEVER
+ON CODE COMMENTS.
+*/
